3a55db28a63bb79270609bb713d1b4a13b625199
github username <github email address>
2016-12-01 12:38:43 -0500

    Ruby Objects Has Many lab

---
+++ lib/post.rb
@@ -0,0 +1,22 @@
+class Post
+
+	attr_accessor :author 
+	attr_reader :title
+
+
+	def initialize(title)
+		@title = title
+	end
+
+
+	def author_name
+		if author 
+			author.name 
+		else 
+			nil 
+		end
+	end
+
+
+end 
+
